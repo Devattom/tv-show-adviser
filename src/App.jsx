74c6132fda462897cffc24e5043b75fd55b360f1
Rousselin <devattom@gmail.com>
2023-05-22 18:26:51 +0200

    Ajout du layout de base sur la partie App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-export function App(){
-    return(
-        <div>Hello World</div>
-    )
+import "./global.css";
+import s from "./style.module.css";
+
+export function App() {
+    return (
+        <div className={s.main_container}>
+            <div className={s.header}>
+                <div className="row">
+                    <div className="col-4">
+                        <div>Logo</div>
+                        <div>Subtitle</div>
+                    </div>
+                    <div className="col-sm-12 col-md-4">
+                        <input style = {{width: "100%"}} type="text" name="" id="" />
+                    </div>
+                </div>
+            </div>
+            <div className={s.tv_show_detail}>detail</div>
+            <div className={s.recommendations}>reco</div>
+        </div>
+    );
 }
