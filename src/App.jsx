0d2837cf789396f4db4d160cd0e749a7ea041a78
Rousselin <devattom@gmail.com>
2023-05-22 21:46:27 +0200

    Ajout useEffect pour appel API et definition de currentTVSHOW

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import { TVShowAPI } from "./api/tv-show";
 import "./global.css";
 import s from "./style.module.css";
+import { BACKDROP_BASE_URL } from "./config";
 
-TVShowAPI.fetchPopulars()
+TVShowAPI.fetchPopulars();
 export function App() {
+    const [currentTVShow, setCurrentTVShow] = useState();
+
+    async function fetchPopulars() {
+        const populars = await TVShowAPI.fetchPopulars();
+        if (populars.length > 0) {
+            setCurrentTVShow(populars[0]);
+        }
+    }
+    useEffect(() => {
+        fetchPopulars();
+    }, []);
+
+    console.log(currentTVShow);
     return (
-        <div className={s.main_container}>
+        <div
+            className={s.main_container}
+            style={{
+                background: currentTVShow ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover` : "black",
+            }}
+        >
             <div className={s.header}>
                 <div className="row">
                     <div className="col-4">
@@ -13,7 +33,12 @@ export function App() {
                         <div>Subtitle</div>
                     </div>
                     <div className="col-sm-12 col-md-4">
-                        <input style = {{width: "100%"}} type="text" name="" id="" />
+                        <input
+                            style={{ width: "100%" }}
+                            type="text"
+                            name=""
+                            id=""
+                        />
                     </div>
                 </div>
             </div>
